74c5b3f74594a8f2dfb04e5fb8fe5e2bf17fa280
aanyakreddy <157770626+aanyakreddy@users.noreply.github.com>
2024-01-25 19:56:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let day=prompt("Enter a Color of the Rainbow");
+switch(day){
+    case "red":console.log("Sunday")
+    break;
+    case "orange":console.log("Monday")
+    break;
+    case "yellow":console.log("Tuesday")
+    break;
+    case "green":console.log("Wednesday")
+    break;
+    case "blue": console.log("Thursday")
+    break;
+    case "purple":console.log("Friday")
+    break;
+    case "indigo":console.log("Saturday")
+    break;
+    default:console.log("Invalid Option")
+}
